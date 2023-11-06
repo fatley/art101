9ac4e9f224b6c21bea7ebc1bf3473d4566f166eb
Ashley Pham <phamashley214@gmail.com>
2023-11-06 14:44:08 -0800

    lab 7

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -7,16 +7,6 @@
 // Functions
 
 
-// Create a new function in your file, call it what you like, but choose wisely.
-// In the function, declare a variable userName and use window.prompt() to get the user's name from the user.
-// Here's some helpLinks to an external site..
-// Sort the letters of the user's name and return those from the function. 
-// Hint: You can't sort strings, but you can sort arrays. Can you convert it back and forth?
-// Make sure your function uses return to return the results.
-// Outside of the function, call the function and output the results with document.writeln() 
-// to neatly output the user's sorted name.
-// Strings print nicely on your webpage, so you don't have to use JSON.stringify() this time.
-
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction() {
 
