c5702be598bc7118994ac2b419b1887bb25f9734
Ashley Pham <phamashley214@gmail.com>
2023-11-13 15:39:10 -0800

    lab 10

---
+++ lab10 copy/js/lab.js
@@ -0,0 +1,46 @@
+// index.js - Using jQuery, appends new elements to an output div
+// Author: Ashley Pham
+// Date:11/13/2023
+
+function generateRandomText() {
+  const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+  const min = 3;
+  const max = 100;
+  const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
+  // Get a random starting index to slice the Lorem Ipsum text
+  const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
+  // Generate the random Lorem Ipsum-like text
+  return text.slice(randStart, randStart + randLen);
+}
+
+const sentences = [
+  "I don't understand what you're saying...",
+  "What???",
+  "I'm confused...",
+  "Is there a translator nearby?",
+  "Huh?"
+];
+
+function randReply() {
+  const index = Math.floor(Math.random() * sentences.length);
+  return sentences[index];
+}
+
+// click listener for button
+// $("#make-convo").click(function(){
+//   const newText = generateRandomText();
+//   $("#output").append('<div class="text"><p>' + newText + '</p></div>');
+// });
+
+let isLeft = true;
+
+$("#make-convo").click(function(){
+  const newText = generateRandomText();
+  const newReply = randReply();
+  if (isLeft) {
+    $("#output").append('<div class="text-left"><p>' + newText + '</p></div>');
+  } else {
+    $("#output").append('<div class="text-right"><p>' + newReply + '</p></div>');
+  }
+  isLeft = !isLeft;
+});
--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -1,41 +1,24 @@
-// index.js - Lab 9 JS file - dealing w/ libraries and jquery
-// Author: Ashley Pham
-// Date: Nov 6, 2023
+// index.js - Using jQuery, appends new elements to an output div
+// Author: Michelle Wang
+// Date:11/13/2023
 
-$(document).ready(function() {
-  $('.challenge').click(function() {
-      var challengeText = 'The challenge of this lab to to understand JavaScript libraries and to practice using them.'; // Replace this with the actual challenge text
-      var newParagraph = $('<p></p>').text(challengeText);
-      $(this).parent().append(newParagraph);
-  });
-});
-
-$(document).ready(function(){
-  $('.problem').click(function(){
-    var problemText = 'Some of the issues we ran into was trying to get the button to display the text. It is a bit confusing at first, but by initializing the text, and a new paragraph variable, we are able to use the append() function to add the paragraph after the user clicks the button.';
-    var newParagraph = $('<p></p>').text(problemText);
-    $(this).parent().append(newParagraph);
-  });
-});
-
-$(document).ready(function(){
-  $('.reflection').click(function(){
-    var reflectionText = 'This lab showed me how important JS libraries can be. Different libraries can help with modularization and help with readability. It is a little overwhelming to learn all the content in the different libraries, but it is important to know how to use them.';
-    var newParagraph = $('<p></p>').text(reflectionText);
-    $(this).parent().append(newParagraph);
-  })
-});
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+function generateRandomText() {
+  const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+  const min = 3;
+  const max = 100;
+  const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
+  // Get a random starting index to slice the Lorem Ipsum text
+  const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
+  // Generate the random Lorem Ipsum-like text
+  return text.slice(randStart, randStart + randLen);
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+// click listener for button
+$("#make-convo").click(function(){
+});
 
-// let's get this party started
-main();
+// get new fake dialogue
+const newText = generateRandomText();
+
+// append a new div to our output div
+$("#output").append('<div class="text"><p>' + newText + '</p></div>');
